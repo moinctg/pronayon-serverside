3612a4e77e16140f0314baa5e3e6c21cf73cf163
Moinuddin Kamal <IT@pronayonbd.com>
2023-01-18 17:53:30 +0600

    post method not working

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,18 +1,38 @@
 
 const asyncHandeler = require('express-async-handler')
 const Project = require('../models/projectModel')
+
 const getProject = asyncHandeler(async(req,res) =>{
     const projects = await Project.find()
     res.status(200).json(projects)
 })
 const postProject = asyncHandeler(async(req,res) =>{
-    if(!req.body.text){
+    if(!req.body){
         res.status(400)
         throw new Error ('pls add a file');
     }
-    res.status(200).json({
-        message:"Post new  Project "
+    const projects = await Project.create({
+        title: req.body.title,
+        description: req.body.description,
+        // class: req.body.class,
+        // client: req.body.client,
+        // Location: req.body.Location,
+        // Consultant: req.body.Consultant,
+        // architect: req.body.architect,
+        // img: req.body.img
+
+        // const newUser = req.body;
+        // const result =  await usersCollection.insertOne(newUser)
+        // console.log('got new user',req.body);
+        // console.log('added new suer',result);
+        // res.json(result);
+
+        
+
+
+      
     })
+    res.status(200).json(projects)
 })
 const putProject = asyncHandeler(async(req,res) =>{
     res.status(200).json({
